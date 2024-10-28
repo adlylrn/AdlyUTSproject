ba75f1c77449dbe3769ece143df989b512b522a7
adlylrn <juliartalerian@gmail.com>
2024-10-28 18:00:07 +0700

    initial commit

---
+++ app/src/main/java/lat/pam/utsproject/OrderActivity.kt
@@ -0,0 +1,27 @@
+package lat.pam.utsproject
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order)
+
+        val namaMakanan = intent.getStringExtra("NAMA_MAKANAN")
+
+        val textOrder = findViewById<TextView>(R.id.etFoodName)
+        textOrder.text = "Anda memesan $namaMakanan"
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
